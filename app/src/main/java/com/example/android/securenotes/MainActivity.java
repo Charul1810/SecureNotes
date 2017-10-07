0611a918278fe029103259f4cb6476170d93339c
Charul1810 <charuldalvi18@gmail.com>
2017-10-07 23:33:44 +0530

    Solved onClick event

--- app/src/main/java/com/example/android/securenotes/MainActivity.java
+++ app/src/main/java/com/example/android/securenotes/MainActivity.java
@@ -2,16 +2,16 @@ package com.example.android.securenotes;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -20,12 +20,18 @@ import android.widget.Toast;
 
 import java.util.List;
 
+import static com.example.android.securenotes.R.id.fab;
+
 public class MainActivity extends AppCompatActivity {
-    EditText note_id,title,note;
+    EditText note_id, title, note;
     ListView listView;
     List<note> mylist;
     DatabaseHandler db;
     AppAdapter adapter;
+    private CoordinatorLayout cdl;
+    private boolean isOpen = false;
+    ListView list;
+    int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,19 +45,31 @@ public class MainActivity extends AppCompatActivity {
         note = (EditText) findViewById(R.id.note);
         listView = (ListView) findViewById(R.id.list_item);
         db = new DatabaseHandler(this);
-
+        cdl = (CoordinatorLayout) findViewById(R.id.actioncontainer);
 
         load();
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab1 = (FloatingActionButton) findViewById(fab);
+        fab1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            startActivity(new Intent(getApplicationContext(),add_new_note.class));
+                //viewMenu();
+                startActivity(new Intent(getApplicationContext(), add_new_note.class));
             }
         });
+
+    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    Toast.makeText(getApplicationContext(), "hello", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(),ViewNote.class));
+                }
+        });
+
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -74,16 +92,18 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    public void load(){
+
+    public void load() {
 
         List<note> list = db.getAllNotes();
         mylist = list;
-        adapter =new  AppAdapter();
+        adapter = new AppAdapter();
         this.listView.setAdapter(adapter);
 
-        Toast.makeText(getApplicationContext(), mylist.size()+"", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), mylist.size() + "", Toast.LENGTH_LONG).show();
 
     }
+
     class AppAdapter extends BaseAdapter {
 
         @Override
@@ -103,8 +123,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         @Override
-        public View getView(final int position, View convertView, ViewGroup parent)
-        {
+        public View getView(final int position, View convertView, ViewGroup parent) {
             if (convertView == null) {
                 convertView = View.inflate(getApplicationContext(),
                         R.layout.list_item, null);
@@ -115,16 +134,19 @@ public class MainActivity extends AppCompatActivity {
             holder.tv_id.setText(mylist.get(position).get_id() + "");
             holder.tv_name.setText(mylist.get(position).get_title());
             holder.tv_num.setText(mylist.get(position).get_note());
+//This code is working but temporary disables to try something
 
-
-            holder.row.setOnClickListener(new View.OnClickListener() {
+           /* holder.row.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    note_id.setText(mylist.get(position).get_id()+"");
+     Toast.makeText(getApplicationContext(),"hello",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), add_new_note.class));
+                   // note_id.setText(mylist.get(position).get_id() + "");
                     title.setText(mylist.get(position).get_title());
                     note.setText(mylist.get(position).get_note());
+
                 }
-            });
+            });*/
 
             return convertView;
         }
--- app/src/main/java/com/example/android/securenotes/add_new_note.java
+++ app/src/main/java/com/example/android/securenotes/add_new_note.java
@@ -1,14 +1,13 @@
 package com.example.android.securenotes;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-
-import java.util.List;
 
 public class add_new_note extends AppCompatActivity {
 
@@ -21,14 +20,25 @@ public class add_new_note extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_note);
 
-       add_note= (Button) findViewById(R.id.add_note);
+     //  add_note= (Button) findViewById(R.id.add_note);
        note_id = (EditText) findViewById(R.id.note_id);
         title = (EditText) findViewById(R.id.title);
         note = (EditText) findViewById(R.id.note);
         listView = (ListView) findViewById(R.id.list_item);
         db = new DatabaseHandler(this);
 
-        add_note.setOnClickListener(new View.OnClickListener() {
+       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab1);
+       fab.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               db.addNote(new note(title.getText().toString(), note.getText().toString()));
+               //Toast.makeText(getApplicationContext(), "note saved successfully!", Toast.LENGTH_LONG).show();
+               clear();
+               startActivity(new Intent(getApplicationContext(),MainActivity.class));
+           }
+       });
+
+        /*add_note.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -37,7 +47,7 @@ public class add_new_note extends AppCompatActivity {
                 clear();
                 startActivity(new Intent(getApplicationContext(),MainActivity.class));
             }
-        });
+        });*/
 
 
     }
