2ef85b66dea47368e873498443fc7dd400d97c93
Charul1810 <charuldalvi18@gmail.com>
2017-10-29 16:43:49 +0530

    splash screen done

---
+++ app/src/main/java/com/example/android/securenotes/Splash.java
@@ -0,0 +1,38 @@
+package com.example.android.securenotes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class Splash extends AppCompatActivity {
+    private Handler mHandler = new Handler();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        startLoadingData();
+
+    }
+    private void startLoadingData() {
+        // finish "loading data" in a random time between 1 and 3 seconds
+        Random random = new Random();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                onLoadingDataEnded();
+            }
+        }, 1000 + random.nextInt(2000));
+    }
+    private void onLoadingDataEnded()
+    {
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+        finish();
+    }
+
+}
